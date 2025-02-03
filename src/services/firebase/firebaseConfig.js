e921a829881098c7501137e210ea82eee1955031
kel <kelabrantes.me@gmail.com>
2025-02-03 14:06:54 -0300

    adicionamento de variaveis de ambiente

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -5,10 +5,10 @@ import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyAcTKUwQ9tOThL4beO7gpzDDS-Bk5_9Wzs",
   authDomain: "rezenha-280c1.firebaseapp.com",
-  projectId: "rezenha-280c1",
-  storageBucket: "rezenha-280c1.firebasestorage.app",
-  messagingSenderId: "275680845444",
-  appId: "1:275680845444:web:baea4f40d544f280ebecd1"
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);

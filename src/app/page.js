a0a77be06d250565a8ce8a36cb211801cc0a50f4
kel <kelabrantes.me@gmail.com>
2025-02-03 10:04:03 -0300

    tirando regras e @ de usuários

--- src/app/page.js
+++ src/app/page.js
@@ -19,16 +19,16 @@ export default function Home() {
         <div className="flex flex-col items-center pt-3 max-w-96 ">
           <h1 className="text-primary font-bold text-lg">Bem Vindo ao REZENHA</h1>
           <div className="w-2/3">
-            <p className="mt-2 text-[14px] text-center font-medium text-terc">Um jogo pra tirar todo mundo do celular e dar umas risadas ou... quem sabe algo a mais </p>
+            <p className="mt-2 text-[14px] text-center font-medium text-terc">Um jogo de eu nunca pra tirar todo mundo do celular e dar umas risadas ou... quem sabe algo a mais </p>
           </div>
           <div className="mt-8 w-1/3 ">
             <Link href='/games' className=""><button className="bg-primary text-white w-full p-2 font-medium rounded-xl text-xl">Jogar</button></Link>
           </div>
 
-          <div className="mt-20 flex flex-col items-center">
+          {/* <div className="mt-20 flex flex-col items-center">
             <Link href='#' className="text-[14px] font-bold underline">Regras do jogo</Link>
             <Link href='#' className="text-[14px] font-bold underline">@ dos jogadores</Link>
-          </div>
+          </div> */}
           
         </div>
 

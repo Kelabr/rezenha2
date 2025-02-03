c03f391fc522ef2fb806019e688bb0a0b446ba98
kel <kelabrantes.me@gmail.com>
2025-02-03 14:21:55 -0300

    arrumando variaveis de ambiente

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -5,7 +5,7 @@ import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: "AIzaSyAcTKUwQ9tOThL4beO7gpzDDS-Bk5_9Wzs",
   authDomain: "rezenha-280c1.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
   appId: process.env.APP_ID
